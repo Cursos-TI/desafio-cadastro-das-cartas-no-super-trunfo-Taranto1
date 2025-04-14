90887de8f712e896327ad64b8bba891110d1581e
Marcelo Taranto <133310368+Taranto1@users.noreply.github.com>
2025-04-14 21:24:32 +0000

    Correções de bugs

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -114,18 +114,6 @@ int main() {
     printf("Área: %.2f km²\n", area_cid_02); 
     printf("PIB: %.2f bilhões de Reais\n", PIB_cid_02); 
     printf("Números de Pontos Turísticos: %d\n", ponto_turistico_cid_02); 
-
-
     
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
-
     return 0;
 }
